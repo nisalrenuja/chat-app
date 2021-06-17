66fa503e162318bf87534cbfa1be473b9870be85
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-06-18 04:07:29 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
  import { ChatEngine } from 'react-chat-engine';
 
  import './App.css';
+
+ const App = () => {
+     return(
+         <ChatEngine
+         height="100vh"
+         projectID="1215fbbf-6470-4f30-9f4d-170f93c8377b"
+         userName="nisal.97"
+         userSecret="Nisal123@"
+         />
+     )
+ }
+
+ export default App;
