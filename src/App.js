1d731e09f07a9a05ac10aec0a02f768673e023f1
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-06-18 15:08:10 +0530

    components initialed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
  import { ChatEngine } from 'react-chat-engine';
 
+ import  ChatFeed  from './components/ChatFeed';
+
  import './App.css';
 
  const App = () => {
@@ -9,8 +11,9 @@
          projectID="1215fbbf-6470-4f30-9f4d-170f93c8377b"
          userName="nisal.97"
          userSecret="Nisal123@"
+         renderChatFeed={(chatAppProps) => <ChatFeed{...chatAppProps}/> }
          />
-     )
+     );
  }
 
  export default App;
