c4ea969cd583d69bacb046b03fee3b593507f291
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-06-19 18:03:24 +0530

    login form styles changed

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -35,7 +35,7 @@ const Modal = () => {
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">Chat Application</h1>
+        <h1 className="title">Chat App By Nisal</h1>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
